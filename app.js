66863bf6a96a10eadc4f8d7d0ef886f24c5a6557
messickr <rnmessick@gmail.com>
2019-06-17 22:56:53 -0700

    got images to display randomly, counting clicks

--- app.js
+++ app.js
@@ -54,13 +54,20 @@ divide objects's times clicked by total shown
 
 // var randomProductImagesArray = ['assets/bag.jpg', 'assets/banana.jpg','assets/bathroom.jpg','assets/boots.jpg','assets/breakfast.jpg', 'assets/bubblegum.jpg','assets/chair.jpg','assets/cthulhu.jpg','assets/dog-duck.jpg','assets/dragon.jpg','assets/pen.jpg','assets/pet-sweep.jpg','assets/scissors.jpg','assets/shark.jpg','assets/sweep.png','assets/tauntaun.jpg','assets/unicorn.jpg','assets/usb.gif','assets/water-can.jpg','assets/wine-glass.jpg'];
 
-// var productImageSectionTag = document.getElementById('allProductImages');
-var productImageA = document.getElementById('productImageA_img');
-var productImageB = document.getElementById('productImageB_img');
-// var productImageC = document.getElementById('productImageC');
+var productImageSectionTag = document.getElementById('allProductImages');
+var leftProductImageTag = document.getElementById('leftProductImage');
+var centerProductImageTag = document.getElementById('centerProductImage');
+var rightProductImageTag = document.getElementById('rightProductImage');
 
 var allProducts = [];
-// var totalClicks = 0;
+var totalClicks = 0;
+
+//Store the images on the page
+
+var leftProductOnPage = null;
+var centerProductOnPage = null;
+var rightProductOnPage = null;
+
 //constructor
 var ProductImage = function(name,imgSrc){
   this.name = name;
@@ -71,25 +78,69 @@ var ProductImage = function(name,imgSrc){
 };
 
 
-var renderNewProducts = function(imgIdxA,imgIdxB){
-  productImageA.src = allProducts[imgIdxA].url;
-  productImageB.src = allProducts[imgIdxB].url;
-  // productImageC.src = ProductImage.allProducts[imgIdxC].url;
+var renderNewProducts = function(leftIndex, centerIndex, rightIndex){
+  leftProductImageTag.src = allProducts[leftIndex].url;
+  centerProductImageTag.src = allProducts[centerIndex].url;
+  rightProductImageTag.src = allProducts[rightIndex].url;
 };
 
 var pickRandomProduct = function(){
-  var imgIdxA = Math.round(Math.random() * allProducts.length - 1);
+  var leftIndex = Math.round(Math.random() * allProducts.length - 1);
 
   do {
-    var imgIdxB = Math.round(Math.random() * allProducts.length - 1);
-  } while (imgIdxA === imgIdxB);
+    var centerIndex = Math.round(Math.random() * allProducts.length - 1);
+    var rightIndex = Math.round(Math.random() * allProducts.length - 1);
+  } while (leftIndex === centerIndex && rightIndex);
   console.log('working so far');
 
-  productImageA = allProducts[imgIdxA];
-  productImageB = allProducts[imgIdxB];
+  leftProductOnPage = allProducts[leftIndex];
+  centerProductOnPage = allProducts[centerIndex];
+  rightProductOnPage = allProducts[rightIndex];
 
-  renderNewProducts(imgIdxA,imgIdxB);
+  renderNewProducts(leftIndex, centerIndex, rightIndex);
+  console.log(allProducts);
 };
+
+//setting up event handler
+
+var handleClickOnProduct = function(event){
+  console.log('let\'s pick some white elephant gifts');
+
+  if(totalClicks < 25){
+    var productChoiceClick = event.target;
+    var id = productChoiceClick.id;
+
+    if(id === 'leftProductImage' || id === 'centerProductImage' || id === 'rightProductImage'){
+      //tracking the products and incrementing clicks
+      if(id === 'leftProductImage'){
+        leftProductOnPage.clicks++;
+      }
+
+      if(id === 'centerProductImage'){
+        centerProductOnPage.clicks++;
+      }
+      if(id === 'rightProductImage'){
+        rightProductOnPage.click++;
+
+      }
+
+      leftProductOnPage.timesShown++;
+      centerProductOnPage.timesShown++;
+      rightProductOnPage.timeshown++;
+
+      pickRandomProduct();
+    }
+    console.log(event.target.id);
+  }
+  totalClicks++;
+
+  //when user has selected 25 times, end the poll
+  if(totalClicks === 25){
+    productImageSectionTag.removeEventListener('click', handleClickOnProduct);
+  }
+};
+
+productImageSectionTag.addEventListener('click', handleClickOnProduct);
 
 allProducts.push(new ProductImage('Bag', 'assets/bag.jpg'));
 allProducts.push(new ProductImage('Banana', 'assets/banana.jpg'));
